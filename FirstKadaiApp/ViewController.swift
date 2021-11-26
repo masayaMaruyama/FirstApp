8857100276a042a31c805adc342b9ef7be8ec830
masaya.maruyama <yayewanshan2@gmail.com>
2021-11-26 20:55:25 +0900

    初めてのアプリを作成

--- FirstKadaiApp/ViewController.swift
+++ FirstKadaiApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func leftButton(_ sender: Any) {
+        label.text = "左"
+    }
+    
+    @IBAction func centerbutton(_ sender: Any) {
+        label.text = "真ん中"
+    }
+    
+    @IBAction func rightButton(_ sender: Any) {
+        label.text = "右"
+    }
+    
+    
 }
 
